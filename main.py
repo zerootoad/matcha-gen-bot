39e9aeec4acc146a2c79b498b3f0d076f63955bb
zeroo | inject <153208404+zerootoad@users.noreply.github.com>
2024-09-08 21:13:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import discord, json, os
+import discord
+import json
+import os
+import threading
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
 
 current_dir = os.path.dirname(__file__)
 with open(os.path.join(current_dir, 'settings', 'config.json'), 'r', encoding='utf-8') as f:
@@ -10,27 +17,27 @@ with open(os.path.join(current_dir, 'settings', 'config.json'), 'r', encoding='u
 TOKEN = settings['token']
 guild_id = settings['guild']
 
-@app.route('/')
-def home():
-    return "Bot is running!"
-
 bot = discord.Bot(intents=discord.Intents.all(), debug_guilds=[guild_id], auto_sync_commands=True)
 
 @bot.event
 async def on_connect():
     await bot.sync_commands()
-    
     print(f"We have connected as {bot.user} and found {len(bot.all_commands.items())} commands")
-        
-        
-if __name__ == '__main__':
+
+def load_cogs():
     current_dir = os.path.dirname(__file__)
     for filename in os.listdir(os.path.join(current_dir, 'cogs')): 
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
             print(f"Loaded Cog: {filename[:-3]}")
-            
+
+def run_flask():
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)        
-            
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
+    load_cogs()
     bot.run(TOKEN)
